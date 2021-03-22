a2256e1647b9b21bc950f9c0d9e6e664eeaf4ec5
Spivak Alexander <po4ta.polevaya@gmail.com>
2021-03-22 19:56:14 +0300

    added comparison functionsЭ

--- utils.hpp
+++ utils.hpp
@@ -5,4 +5,29 @@
 #ifndef FT_CONTAINERS__UTILS_HPP_
 #define FT_CONTAINERS__UTILS_HPP_
 
+namespace ft {
+
+template<class Arg1, class Arg2, class Result>
+struct binary_function {
+  typedef Arg1 first_argument_type;
+  typedef Arg2 second_argument_type;
+  typedef Result result_type;
+};
+
+template<class T>
+struct less : binary_function<T, T, bool> {
+  bool operator()(const T &x, const T &y) const { return x < y; }
+};
+
+template<class T>
+struct equal_to : binary_function<T, T, bool> {
+  bool operator()(const T &x, const T &y) const { return x == y; }
+};
+
+template <class T> struct greater : binary_function <T,T,bool> {
+  bool operator() (const T& x, const T& y) const {return x>y;}
+};
+
+}
+
 #endif //FT_CONTAINERS__UTILS_HPP_
